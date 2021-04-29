bf0b4bf49a12d05ba3f0d62feb4778d31c4a43d8
Zydhan Linnar Putra <zydhan.19051@mhs.its.ac.id>
2021-04-29 09:56:53 +0700

    Remove placeholder image in dashboard panel

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -6,7 +6,6 @@
         @foreach ($dashboard as $item)
         <div class="col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-6 mb-3">
             <div class="card col-12">
-                <img src="{{ $item["imageURL"] }}" class="card-img-top" alt="{{ $item["slug"] }}-image">
                 <div class="card-body">
                   <h5 class="card-title">{{ $item["title"] }}</h5>
                   <p class="card-text">{{ $item["description"] }}</p>
